0f677a3366149efbdf8bf42a3e39bbd756499f0b
Maurelian <maurelian@protonmail.ch>
2024-10-29 13:07:16 -0400

    fix: bump semvers on new contracts

--- op-chain-ops/foundry/testdata/srcmaps/src/StorageLibrary.sol
+++ op-chain-ops/foundry/testdata/srcmaps/src/StorageLibrary.sol
@@ -1,12 +1,9 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.15;
+pragma solidity 0.8.25;
 
 // @notice StorageLibrary is an example library used for integration testing.
 library StorageLibrary {
-
     function addData(uint256 _data) internal pure returns (uint256) {
         return _data + 123;
     }
-
 }
-
--- op-chain-ops/script/testdata/scripts/ScriptExample.s.sol
+++ op-chain-ops/script/testdata/scripts/ScriptExample.s.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.15;
+pragma solidity 0.8.25;
 
 // Vm is a minimal interface to the forge cheatcode precompile
 interface Vm {
