eaf9964f719555e0c3c6fd96e6db76aa5dd5df3b
Maurelian <maurelian@protonmail.ch>
2024-11-01 14:42:52 -0400

    fix: TestInteropDeployment

--- op-deployer/pkg/deployer/integration_test/apply_test.go
+++ op-deployer/pkg/deployer/integration_test/apply_test.go
@@ -5,7 +5,6 @@ import (
 	"context"
 	"encoding/hex"
 	"fmt"
-	"github.com/ethereum-optimism/optimism/op-chain-ops/foundry"
 	"log/slog"
 	"math/big"
 	"os"
@@ -13,6 +12,7 @@ import (
 	"time"
 
 	altda "github.com/ethereum-optimism/optimism/op-alt-da"
+	"github.com/ethereum-optimism/optimism/op-chain-ops/foundry"
 	"github.com/ethereum-optimism/optimism/op-deployer/pkg/deployer/inspect"
 	"github.com/ethereum-optimism/optimism/op-node/rollup"
 
@@ -415,7 +415,7 @@ func TestInteropDeployment(t *testing.T) {
 	chainState := st.Chains[0]
 	depManagerSlot := common.HexToHash("0x1708e077affb93e89be2665fb0fb72581be66f84dc00d25fed755ae911905b1c")
 	checkImmutable(t, st.L1StateDump.Data.Accounts, st.ImplementationsDeployment.SystemConfigImplAddress, depManagerSlot)
-	proxyAdminOwnerHash := common.BytesToHash(intent.Chains[0].Roles.SystemConfigOwner.Bytes())
+	proxyAdminOwnerHash := common.BytesToHash(intent.Chains[0].Roles.L1ProxyAdminOwner.Bytes())
 	checkStorageSlot(t, st.L1StateDump.Data.Accounts, chainState.SystemConfigProxyAddress, depManagerSlot, proxyAdminOwnerHash)
 }
 
