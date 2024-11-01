c20eed3f02f2247f8c480b88b4eb30675eecfc62
Matthew Slipper <me@matthewslipper.com>
2024-11-01 10:47:50 -0600

    Add ArtifactsFromURL utility

--- op-deployer/pkg/deployer/testutil/env.go
+++ op-deployer/pkg/deployer/testutil/env.go
@@ -8,26 +8,30 @@ import (
 	"runtime"
 	"testing"
 
-	artifacts2 "github.com/ethereum-optimism/optimism/op-deployer/pkg/deployer/artifacts"
+	"github.com/ethereum-optimism/optimism/op-deployer/pkg/deployer/artifacts"
 
 	"github.com/ethereum-optimism/optimism/op-chain-ops/foundry"
 	op_service "github.com/ethereum-optimism/optimism/op-service"
 	"github.com/stretchr/testify/require"
 )
 
-func LocalArtifacts(t *testing.T) (*artifacts2.Locator, foundry.StatDirFs) {
+func LocalArtifacts(t *testing.T) (*artifacts.Locator, foundry.StatDirFs) {
 	_, testFilename, _, ok := runtime.Caller(0)
 	require.Truef(t, ok, "failed to get test filename")
 	monorepoDir, err := op_service.FindMonorepoRoot(testFilename)
 	require.NoError(t, err)
 	artifactsDir := path.Join(monorepoDir, "packages", "contracts-bedrock", "forge-artifacts")
-	artifactsURL, err := url.Parse(fmt.Sprintf("file://%s", artifactsDir))
+	return ArtifactsFromURL(t, fmt.Sprintf("file://%s", artifactsDir))
+}
+
+func ArtifactsFromURL(t *testing.T, artifactsURLStr string) (*artifacts.Locator, foundry.StatDirFs) {
+	artifactsURL, err := url.Parse(artifactsURLStr)
 	require.NoError(t, err)
-	loc := &artifacts2.Locator{
+	loc := &artifacts.Locator{
 		URL: artifactsURL,
 	}
 
-	artifactsFS, cleanupArtifacts, err := artifacts2.Download(context.Background(), loc, artifacts2.NoopDownloadProgressor)
+	artifactsFS, cleanupArtifacts, err := artifacts.Download(context.Background(), loc, artifacts.NoopDownloadProgressor)
 	require.NoError(t, err)
 	t.Cleanup(func() {
 		_ = cleanupArtifacts()
